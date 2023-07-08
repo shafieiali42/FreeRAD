d1095b3b404e0d834bdfd488a46d630515a49d38
shafieiali42 <62015693+shafieiali42@users.noreply.github.com>
2023-07-09 00:08:30 +0330

    working on training part

--- train/training2.py
+++ train/training2.py
@@ -15,6 +15,9 @@ from model.fp16_util import (
     zero_grad,
 )
 from model.nn import update_ema
+from tqdm import tqdm
+
+
 
 
 EPOCHS=20
@@ -24,16 +27,34 @@ class TrainLoop:
         unet,
         diffusion,
         train_dataLoader,
+        batch_size,
         lr,
         ema_rate,
-        weight_decay=0.0
+        device,
+        weight_decay=0.0,
     ):
         self.unet=unet
         self.diffusion=diffusion
         self.train_dataLoader=train_dataLoader
+        self.batch_size=batch_size
         self.lr=lr
         self.ema_rate=ema_rate
+        self.device=device
         self.weight_decay=weight_decay
         self.optimizer = AdamW(self.unet.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         for epoch in range(EPOCHS):
-            pass
+            for batch in tqdm(train_dataLoader):
+                print(f"Epoch: {epoch}")
+                batch=batch.to(device)
+                self.optimizer.zero_grad()
+                t=np.random.uniform(0,self.diffusion.num_timesteps,size=(self.batch_size,))
+                losses=self.diffusion.training_losses(self.unet,batch,t)
+                weights=np.ones([self.batch_size])
+                weights = th.from_numpy(weights).float().to(device)
+                loss = (losses["loss"] * weights).mean() ## todo
+                loss.backward()
+                self.optimizer.step()
+      
+                
+
+            
