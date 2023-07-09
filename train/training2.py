cac40d0a65e5287b09b6fbf8557cf9927af30bf5
it <alizaden.1379@gmail.com>
2023-07-10 01:31:00 +0430

    hyperparameters updated, bugs fixed

--- train/training2.py
+++ train/training2.py
@@ -34,24 +34,28 @@ class TrainLoop:
         self.ema_rate=ema_rate
         self.device=device
         self.weight_decay=weight_decay
-        ema = EMA(0.995)
-        ema_model = copy.deepcopy(self.unet).eval().requires_grad_(False)
+        self.ema = EMA(0.995)
+        self.ema_model = copy.deepcopy(self.unet).eval().requires_grad_(False)
         self.optimizer = AdamW(self.unet.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        for epoch in range(num_epochs):
-            print(f"Epoch: {epoch}/{num_epochs}")
-            for batch in tqdm(train_dataLoader):
-                batch=batch.to(device)
+        self.num_epochs=num_epochs
+        self.base_model_path=base_model_path
+
+    def run_loop(self):
+        for epoch in range(self.num_epochs):
+            print(f"Epoch: {epoch}/{self.num_epochs}")
+            for batch in tqdm(self.train_dataLoader):
+                batch=batch.to(self.device)
                 self.optimizer.zero_grad()
                 t=np.random.uniform(0,self.diffusion.num_timesteps,size=(self.batch_size,))
-                t = th.from_numpy(t).long().to(device)
+                t = th.from_numpy(t).long().to(self.device)
                 losses=self.diffusion.training_losses(self.unet,batch,t)
                 weights=np.ones([self.batch_size])
-                weights = th.from_numpy(weights).float().to(device)
+                weights = th.from_numpy(weights).float().to(self.device)
                 loss = (losses["loss"] * weights).mean() ## todo
                 loss.backward()
                 print(losses)
                 print("-"*500)
                 print(loss)
                 self.optimizer.step()
-                ema.step_ema(ema_model, self.unet)
-            th.save({'epoch': epoch,'model_state_dict': self.unet.state_dict(),'optimizer_state_dict': self.optimizer.state_dict(),}, base_model_path+f'checkpoint_ep{epoch}.pt')
+                self.ema.step_ema(self.ema_model, self.unet)
+            th.save({'epoch': epoch,'model_state_dict': self.unet.state_dict(),'optimizer_state_dict': self.optimizer.state_dict(),}, self.base_model_path+f'checkpoint_ep{epoch}.pt')
