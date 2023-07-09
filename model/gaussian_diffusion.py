3074c2d41afc89da06404c97ac12eddf5b97aaae
shafieiali42 <62015693+shafieiali42@users.noreply.github.com>
2023-07-10 01:47:39 +0330

    fix bugs

--- model/gaussian_diffusion.py
+++ model/gaussian_diffusion.py
@@ -202,7 +202,7 @@ class GaussianDiffusion:
         if noise is None:
             noise = th.randn_like(x_start)
         assert noise.shape == x_start.shape
-        print(type(t))
+        # print(type(t))
         return (
             _extract_into_tensor(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start
             + _extract_into_tensor(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape)
