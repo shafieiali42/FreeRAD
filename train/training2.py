131bc775ced367ad8b218bc4f9d0505434d3a5ff
shafieiali42 <62015693+shafieiali42@users.noreply.github.com>
2023-07-11 13:31:29 +0330

    change saving part

--- train/training2.py
+++ train/training2.py
@@ -78,9 +78,10 @@ class TrainLoop:
                 self.ema.step_ema(self.ema_model, self.unet)
                 train_loss+=loss
                 iter_cnt+=1
+                if self.step%1000==0:
+                    print(f"Step: {self.step}")
+                if self.step%10000==0:
+                    th.save({'epoch': epoch,'step':self.step,'model_state_dict': self.unet.state_dict(),'optimizer_state_dict': self.optimizer.state_dict(),'loss': train_loss/iter_cnt,}, self.base_model_path+f'checkpoint_ep{epoch}.pt')
             epoch_loss=train_loss/iter_cnt
             print(f'Train Loss: {epoch_loss:.4f}')
-            if self.step%1000==0:
-                print(f"Step: {self.step}")
-            if self.step%10000==0:
-                th.save({'epoch': epoch,'step':self.step,'model_state_dict': self.unet.state_dict(),'optimizer_state_dict': self.optimizer.state_dict(),'loss': epoch_loss,}, self.base_model_path+f'checkpoint_ep{epoch}.pt')
+            
